b0307c088206da45cc681ae2b6fd887625455835
Gregor Vidmar <gregor.vidmar@gmail.com>
2020-09-24 00:06:48 +0200

    Responsive Most read Visitor count Image opt 1.0 controller fix api sync

--- website_ascaldera/controllers/main.py
+++ website_ascaldera/controllers/main.py
@@ -10,11 +10,14 @@ from odoo import http
 from odoo.addons.http_routing.models.ir_http import slug
 from odoo.addons.website.controllers.main import Website
 from odoo.addons.website_blog.controllers.main import WebsiteBlog
+from odoo.addons.web.controllers.main import Binary
 from odoo.http import request
 import base64
 from bs4 import BeautifulSoup
 import logging
 import json
+import odoo
+from odoo.tools import crop_image, topological_sort, html_escape, pycompat
 
 def check_lang_to_installed(env, website):
     for code in ['sl_SI', 'it_IT', 'en_US']:
@@ -31,6 +34,72 @@ def install_lang(env, code):
     env["base.update.translations"].create({'lang': code}).act_update()
     lang = env['res.lang'].search([('code', '=', code)], limit=1)
     return lang
+
+def binary_content(xmlid=None, model='ir.attachment', id=None, field='datas', unique=False,
+                   filename=None, filename_field='datas_fname', download=False, mimetype=None,
+                   default_mimetype='application/octet-stream', related_id=None, access_mode=None, access_token=None,
+                   env=None):
+    return request.registry['ir.http'].binary_content(
+        xmlid=xmlid, model=model, id=id, field=field, unique=unique, filename=filename,
+        filename_field=filename_field, download=download, mimetype=mimetype,
+        default_mimetype=default_mimetype, related_id=related_id, access_mode=access_mode, access_token=access_token,
+        env=env)
+
+
+class Binary(http.Controller):
+
+    @http.route(['/website/image/<model>/<id>/<field>/<int:width>x<int:height>/<string:crop_setting>'
+    ], type='http', auth="public", website=False, multilang=False)
+    def content_imagecontent_image(self, xmlid=None, model='ir.attachment', id=None, field='datas',
+                      filename_field='datas_fname', unique=None, filename=None, mimetype=None,
+                      download=None, width=0, height=0, crop=False, related_id=None, access_mode=None,
+                      access_token=None, avoid_if_small=False, upper_limit=False, signature=False,crop_setting = "crop", **kw):
+
+        status, headers, content = binary_content(
+            xmlid=xmlid, model=model, id=id, field=field, unique=unique, filename=filename,
+            filename_field=filename_field, download=download, mimetype=mimetype,
+            default_mimetype='image/png', related_id=related_id, access_mode=access_mode, access_token=access_token)
+
+        crop = False
+        if crop_setting == 'crop':
+            crop = True
+        else:
+            crop = False
+
+        if headers and dict(headers).get('Content-Type', '') == 'image/svg+xml':  # we shan't resize svg images
+            height = 0
+            width = 0
+        else:
+            height = int(height or 0)
+            width = int(width or 0)
+
+        if not content:
+            content = base64.b64encode(self.placeholder(image='placeholder.png'))
+            headers = self.force_contenttype(headers, contenttype='image/png')
+            if not (width or height):
+                suffix = field.split('_')[-1]
+                if suffix in ('small', 'medium', 'big'):
+                    content = getattr(odoo.tools, 'image_resize_image_%s' % suffix)(content)
+
+        if crop and not(width or height):
+            content = crop_image(content, type='center', size=(width, height), ratio=(1, 1))
+        elif (width or height):
+            if not upper_limit:
+                # resize maximum 500*500
+                if width > 1024:
+                    width = 1024
+                if height > 1024:
+                    height = 1024
+            content = odoo.tools.image_resize_image(base64_source=content, size=(width or None, height or None),
+                                                    encoding='base64', upper_limit=upper_limit,
+                                                    avoid_if_small=avoid_if_small)
+
+        image_base64 = base64.b64decode(content)
+        headers.append(('Content-Length', len(image_base64)))
+        response = request.make_response(image_base64, headers)
+        response.status_code = status
+        return response
+
 
 
 class Website(Website):
@@ -76,42 +145,32 @@ class Website(Website):
         # Get max three news posts
         news_id = request.env.ref(
             'website_ascaldera.blog_post_type_news')
-        news_posts_visits = [p.visits for p in blog_post.sudo().search(
-            [('blog_post_type_id', '=', news_id.id),
-             ('lang', '=', request.env.context.get('lang'))])]
-        news_highest_visits = heapq.nlargest(3, news_posts_visits)
+
         most_read_news_post = blog_post.sudo().search(
             [('blog_post_type_id', '=', news_id.id),
              ('website_published', '=', True),
-             ('visits', 'in', news_highest_visits),
-             ('lang', '=', request.env.context.get('lang'))], limit=3)
+             ('lang', '=', request.env.context.get('lang'))], limit=3,order='visits,published_date desc')
+
+
         # Get max three article posts
         article_id = request.env.ref(
             'website_ascaldera.blog_post_type_article')
-        article_posts_visits = [p.visits for p in blog_post.sudo().search(
-            [('blog_post_type_id', '=', article_id.id),
-             ('lang', '=', request.env.context.get('lang'))])]
-        article_highest_visits = heapq.nlargest(3, article_posts_visits)
+
         most_read_article_post = blog_post.sudo().search(
             [('blog_post_type_id', '=', article_id.id),
              ('website_published', '=', True),
-             ('lang', '=', request.env.context.get('lang')),
-             ('visits', 'in', article_highest_visits)], limit=3)
+             ('lang', '=', request.env.context.get('lang'))], order='visits,published_date desc',limit=3)
         # Get max three practice posts
         practice_id = request.env.ref(
             'website_ascaldera.blog_post_type_judicial_practice')
-        practice_posts_visits = [p.visits for p in blog_post.sudo().search(
-            [('blog_post_type_id', '=', practice_id.id),
-             ('lang', '=', request.env.context.get('lang'))])]
-        practice_highest_visits = heapq.nlargest(3, practice_posts_visits)
+
         most_read_practice_post = blog_post.sudo().search(
             [('blog_post_type_id', '=', practice_id.id),
              ('website_published', '=', True),
-             ('visits', 'in', practice_highest_visits),
-             ('lang', '=', request.env.context.get('lang'))], limit=3)
+             ('lang', '=', request.env.context.get('lang'))], order='visits,published_date desc', limit=3)
         grid_images_posts = blog_post.sudo().search(
             [('website_published', '=', True),
-             ('lang', '=', request.env.context.get('lang'))], limit=7)
+             ('lang', '=', request.env.context.get('lang'))],order='published_date desc', limit=7)
 
         check_lang_to_installed(request.env, request.website)
         
@@ -237,14 +296,24 @@ class WebsiteBlog(WebsiteBlog):
     ], type='http', auth="public", website=True)
     def blog_post(self, blog, blog_post, tag_id=None, page=1, enable_editor=None, **post):
         """Controller to render new template "blog_post_content"."""
-        return request.render("website_ascaldera.blog_post_content", {
+        values = {
             'blog': blog,
             'fav_tags': self.fav_tags_get(),
             'unfav_tags': self.unfav_tags_get(),
             'blog_post': blog_post,
             'main_object': blog_post,
             'external_post_count': self.get_external_post_count(),
-        })
+        }
+        response = request.render("website_ascaldera.blog_post_content", values)
+        request.session[request.session.sid] = request.session.get(request.session.sid, [])
+        if not (blog_post.id in request.session[request.session.sid]):
+            request.session[request.session.sid].append(blog_post.id)
+            # Increase counter
+            blog_post.sudo().write({
+                'visits': blog_post.visits + 1,
+            })
+        return response
+
 
     @http.route([
         '''/blog/post/''',
@@ -392,7 +461,8 @@ class WebsiteBlog(WebsiteBlog):
                 'legislation_foreign': {'sub_category_main': 'foreign_legislation',
                                         'view_id': 'website_ascaldera.blog_post_judgement_SLO'},
                 'edpb_guidelines': {'sub_category_main': 'edpb_guidelines',
-                                        'view_id': 'website_ascaldera.blog_post_edpb_guidelines'}
+                                        'view_id': 'website_ascaldera.blog_post_edpb_guidelines'},
+                'practice_slo':{'sub_category_main':'slo_practice','view_id':'website_ascaldera.website_ascaldera'}
             }
 
         domain = [
@@ -417,10 +487,14 @@ class WebsiteBlog(WebsiteBlog):
             step=8,
         )
         posts = BlogPost.search(domain, offset=(page - 1) * _blog_post_per_page, limit=_blog_post_per_page)
+        most_read_posts = BlogPost.search(domain, limit=4,order='visits,published_date desc')
         subtitle = False
-        if subtype and len(posts):
-            subtitle = dict(BlogPost._fields['sub_category_main']._description_selection(request.env)).get(posts[0].sub_category_main)
-
+        title = ""
+        if blog_type_id and len(blog_type_id):
+            title = blog_type_id.display_name
+        if subtype:
+            subtitle = dict(BlogPost._fields['sub_category_main']._description_selection(request.env)).get(blog_subtypes[subtype]['sub_category_main'])
+            title = subtitle
         render_values = {
                         'page':page,
                         'type':type,
@@ -429,9 +503,11 @@ class WebsiteBlog(WebsiteBlog):
                          'blog_type': blog_type_id,
                          'pager': pager,
                          'posts': posts,
+                         'most_read_posts': most_read_posts,
                          'fav_tags': self.fav_tags_get(),
                          'unfav_tags': self.unfav_tags_get(),
-                         'external_post_count': self.get_external_post_count()
+                         'external_post_count': self.get_external_post_count(),
+                         'additional_title': title
                          }
         return render_values
 
@@ -469,7 +545,7 @@ class WebsiteBlog(WebsiteBlog):
 
 
     @http.route([
-        '/blog/Judicial-Practice/practice_slo',
+        '/blog/Judicial-Practice/practice_slo_old',
     ], type='http', auth="public", website=True)
     def blog_post_legislation(self, **post):
         """Controller for Legislation  page."""
@@ -488,6 +564,7 @@ class WebsiteBlog(WebsiteBlog):
                                      'post_date': parse(res['lastModifiedAt']).strftime('%A, %d. %B %Y'),
                                      'external_post_link': res['_links']['self']['href'],
                                     }})
+
         return request.render("website_ascaldera.blog_post_legislation", {
             'data': vals,
             'fav_tags': self.fav_tags_get(),
--- website_ascaldera/models/website_blog.py
+++ website_ascaldera/models/website_blog.py
@@ -5,10 +5,92 @@ from bs4 import BeautifulSoup
 from odoo import api, fields, models
 from datetime import date,datetime
 
+import requests
+import base64
+from bs4 import BeautifulSoup
+from dateutil.parser import parse
+from odoo.modules.module import get_module_resource
 
 @api.model
 def _lang_get(self):
     return self.env['res.lang'].get_installed()
+
+
+class BlogBlog(models.Model):
+    _inherit = "blog.blog"
+
+    def sync_api_blog(self,language=False,url=False,author_id=False,blog_id=False,blog_type_id=False,subtype_selection=False):
+        blog_post_object = self.env['blog.post']
+        res = requests.get(url)
+        result = 0
+        if (res):
+            result = res.json()
+        records = []
+        count = 0
+        #/website_ascaldera/static/src/img/IP_logo.jpg
+        img_path = get_module_resource('website_ascaldera', 'static/src/img', 'IP_logo.jpg')
+        img_base = False
+        if img_path:
+            with open(img_path, 'rb') as f:
+                image = f.read()
+                img_base = base64.b64encode(image)
+        if (result != 0):
+            for res in result['_embedded']['documents']:
+                count += 1
+                values= {
+                                    'name': res['name'],
+                                    'blog_id': blog_id,
+                                    'blog_post_type_id': blog_type_id.id,
+                                    'sub_category_main':subtype_selection,
+                                    'content': BeautifulSoup(base64.b64decode(res['content']), features="lxml").get_text(),
+                                    'published_date': parse(res['lastModifiedAt']).strftime('%Y-%m-%d %H:%M:%S'),
+                                    'document_date': parse(res['createdAt']).strftime('%Y-%m-%d %H:%M:%S'),
+                                    'website_published':True,
+                                    'api_id': res['id'],
+                                    'api_sync': True,
+                                    'api_link': res['_links']['self']['href'],
+                                    'api_last_modified_at':parse(res['lastModifiedAt']).strftime('%Y-%m-%d %H:%M:%S'),
+                                    'lang':language,
+                                    'author_id':author_id,
+                                    'write_uid':author_id,
+
+                                     }
+
+                if img_base:
+                    values['blog_post_cover_image'] = img_base
+                records.append(values)
+
+        all_blog_post = []
+        for record in records:
+            existing_blog_post = blog_post_object.search([('lang','=',language),('api_sync','=',True),('api_id','=',record['api_id'])],limit=1)
+            if existing_blog_post and len(existing_blog_post):
+                all_blog_post.append(existing_blog_post.id)
+                if record['api_last_modified_at'] > str(existing_blog_post.api_last_modified_at):
+                    existing_blog_post.write(record)
+                else:
+                    existing_blog_post.write(record)
+
+
+            else:
+                blot_post_id = blog_post_object.create(record)
+                blot_post_id.sub_category_main = subtype_selection
+                all_blog_post.append(blot_post_id.id)
+        all_posts = blog_post_object.search(
+            [('lang', '=', language), ('api_sync', '=', True), ('id', 'in', all_blog_post)])
+        all_posts.write( {'website_published': True})
+        for post in all_posts:
+            post.published_date = post.api_last_modified_at
+
+
+
+        blog_post_object.search([('lang', '=', language), ('api_sync', '=', True), ('id','not in', all_blog_post)]).write({'website_published':False})
+
+
+
+    def sync_api_slo_blog(self):
+        blog_type_id = self.env.ref('website_ascaldera.blog_post_type_judicial_practice')
+        self.sync_api_blog('sl_SI','http://staging.app.gdpr.ascaldera.com//api/v1/documents/search?query=zakon&projection=documentDetail',10,5,blog_type_id,'slo_practice')
+
 
 
 class BlogPostType(models.Model):
@@ -36,6 +118,11 @@ class BlogPost(models.Model):
                             help="Select language for blog post content",
                             required=True)
     document_date = fields.Datetime(string="Document Date")
+    api_sync = fields.Boolean(string="Api sync",default=False, readonly=True)
+    api_id = fields.Integer(string="Api Id", readonly=True, index=True)
+    api_link = fields.Char(string="Api link",readonly=True)
+    api_last_modified_at = fields.Datetime('Api last modified at')
+
 
     def get_date(self):
         for r in self:
@@ -48,7 +135,7 @@ class BlogPost(models.Model):
     save_name=fields.Char(string='Saved name')
     
     sub_category_main=fields.Selection(string="Subcategory", 
-                                       selection=[('foreign_legislation','Foreign legislation'),('slovenian_legislation','Slovenian legislation'),('SLO_commissioner_practice','SLO Information Commissioner\'s practice'),('SLO_judgments','Judgments of the Slovenian Court'),('foreign_practice','The practice of foreign oversight bodies'),('EU_judgments','Judgments of the European Court of Justice'),('escp_judgements','ECHR judgments'),('foreign_judgments','Judgments of foreign courts'), ('edpb_guidelines', 'EDPB guidelines and opinions')])
+                                       selection=[('foreign_legislation','Foreign legislation'),('slovenian_legislation','Slovenian legislation'),('SLO_commissioner_practice','SLO Information Commissioner\'s practice'),('SLO_judgments','Judgments of the Slovenian Court'),('foreign_practice','The practice of foreign oversight bodies'),('EU_judgments','Judgments of the European Court of Justice'),('escp_judgements','ECHR judgments'),('foreign_judgments','Judgments of foreign courts'), ('edpb_guidelines', 'EDPB guidelines and opinions'),('slo_practice','SLO Information Commissioners practice')])
     sub_category_1=fields.Selection(string="Subcategory", 
                                     selection=[('SLO_commissioner_practice','SLO Information Commissioner\'s practice'), ('SLO_judgments','Judgments of the Slovenian Court'), ('foreign_practice','The practice of foreign oversight bodies'),('EU_judgments','Judgments of the European Court of Justice'),('escp_judgements','ECHR judgments'),('foreign_judgments','Judgments of foreign courts'), ('edpb_guidelines', 'EDPB guidelines and opinions')])
     sub_category_2=fields.Selection(string="Subcategory", 
